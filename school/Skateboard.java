77cdf6c14a17346c8a8538109198f3ca793c83f5
Thato <219085633@ump.ac.za>
2024-05-07 21:12:11 +0200

    adding the skateboard and SkateboardObject classes

---
+++ school/Skateboard.java
@@ -0,0 +1,15 @@
+private double myBoardLength;
+public Skateboard() {
+myBoardLength = 0;
+}
+public Skateboard(double boardLength) {
+setBoardLength(boardLength);
+}
+public double getBoardLength() {
+return myBoardLength;
+}
+public void setBoardLength(double boardLength){
+myBoardLength= boardLength;
+}
+}
+
